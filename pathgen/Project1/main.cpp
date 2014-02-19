d097e61e5b42c552d3b771d11fe84f3d1eddb24a
Sindre M. Braaten <sindre.mb@gmail.com>
2014-02-19 11:42:14 +0100

    can open data generator / editor in browser from path generator

--- pathgen/Project1/main.cpp
+++ pathgen/Project1/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+
+#include <shlobj.h>
+#include <shlwapi.h>
+#include <objbase.h>
 
 #include "moseldataparser.h"
 #include "entities.h"
@@ -16,7 +21,7 @@ int main()
 	cout << "Welcome to network path generation bot v0.1";
 	while(true) {
 		int rootSelection;
-		cout << "\n\n Menu:\n (1) Load (JSON) file and generate paths\n (0) exit\n\nSelection: ";
+		cout << "\n\n Menu:\n (1) Load (JSON) file and generate paths\n (2) Open editor \n (0) exit\n\nSelection: ";
 		cin >> rootSelection;
 
 		if(rootSelection == 1) {
@@ -40,6 +45,18 @@ int main()
 
 			cout << "\n\nPaths generated and data stored to " << outputfilename;
 
+		} else if(rootSelection == 2) {
+			const char* urlA = "file://C:\Users\sindremo\cloudbrokermodel\datagen\index.htm";
+			wchar_t urlW[150];
+			std::copy( urlA, urlA + lstrlenA( urlA ) + 1, urlW );
+			//if ((int)ShellExecuteW( NULL, L"open", urlW, NULL, NULL, SW_SHOW ) < 32)
+			if ((int)ShellExecute(NULL, "open", "..\\datagen\\index.htm", NULL, NULL, SW_SHOWNORMAL) < 32)
+			{
+				cout << "\n editor opened";
+			}
+			else {
+				cout << "\n could not open editor";
+			}
 		} else if(rootSelection == 0) {
 			break;
 		} else {
